82f53105ee12883925738dbb2ee28a96b729e3d2
alferesm05 <163531657+alferesm05@users.noreply.github.com>
2024-10-16 09:23:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Piada' ,
+    'O que um fisico faz quando está triste?' ,
+    'ele tenta melhorar  seu estado' ,
+)
+
+criaCartao(
+    'piada',
+    'por que a velhinha não usa relógio?' ,
+    'porque ela é sem hora'
+)
+
+criaCartao(
+    'piada' ,
+    'sabe porque o anão foi surfar na cozinha?' ,
+    'porque lá tem microondas'
+)
+
+criaCartao(
+    'Piada' ,
+    'o que é um pontinho vermelho na árvore?' , 
+    'um morangotango'
+)
